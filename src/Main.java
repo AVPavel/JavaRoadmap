d7f89c99596928a2f1913d1d9a1652b0a71be22b
AVPavel <andreipavel56@gmail.com>
2024-11-09 13:54:19 +0200

    https://roadmap.sh/java working with files and API

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
